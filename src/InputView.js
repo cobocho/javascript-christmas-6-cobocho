f2d3e18a5cecbb90ae19ece0a75873bbf74f7223
cobocho <rlaalsrb1111@naver.com>
2023-11-10 19:33:37 +0900

    feat: 기본 `OutputView` 및 `InputView` 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,25 @@
-export default InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+import QUERIES from './constants/queries.js';
+
+const InputView = Object.freeze({
+  /**
+   * 사용자에게 입력값을 받아옵니다.
+   * @param {string} query - 사용자에게 질문할 쿼리입니다.
+   * @returns {Promise<string>} 사용자의 입력값입니다.
+   */
+  async read(query) {
+    const input = await Console.readLineAsync(query);
+    return input;
+  },
+
+  /**
+   * 사용자에게 방문 일자를 받아옵니다.
+   * @returns {Promise<string>} 사용자의 방문일입니다.
+   */
+  async readVisitDate() {
+    const input = await this.read(QUERIES.visitDate);
+    return input;
+  },
+});
+
+export default InputView;
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,29 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+import MESSAGES from './constants/messages.js';
+
+const OutputView = Object.freeze({
+  /**
+   * 사용자에게 콘솔로 메세지를 출력합니다.
+   * @param {string} message - 출력할 문구입니다.
+   */
+  print(message) {
+    Console.print(message);
+  },
+
+  /**
+   * 시작 문구를 출력합니다.
+   */
+  startComment() {
+    this.print(MESSAGES.startComment);
+  },
+
+  /**
+   * @param {string} message - 에러의 메세지입니다.
+   * 에러를 출력합니다.
+   */
+  error(message) {
+    this.print(`\n${message}\n`);
+  },
+});
+
+export default OutputView;
