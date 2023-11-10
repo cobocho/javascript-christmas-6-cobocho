dc7ebbdf0c28be5a7b484334c52da8b67c7f5f5b
cobocho <rlaalsrb1111@naver.com>
2023-11-10 15:11:59 +0900

    feat: `Date` 객체 관련 유틸리티 구현

---
+++ src/utils/date/date.js
@@ -0,0 +1,52 @@
+/**
+ * 입력된 Date들이 같은 년도인지 비교합니다.
+ * @param {Date} date1 원본 날짜입니다.
+ * @param {Date} date2 비교할 날짜입니다.
+ * @returns {boolean} 년도의 동일 여부입니다.
+ */
+export const isSameYear = (date1, date2) => {
+  const originalDateYear = date1.getFullYear();
+  const preparedDateYear = date2.getFullYear();
+
+  return originalDateYear === preparedDateYear;
+};
+
+/**
+ * 입력된 Date들이 같은 월인지 비교합니다.
+ * @param {Date} date1 원본 날짜입니다.
+ * @param {Date} date2 비교할 날짜입니다.
+ * @returns {boolean} 월의 동일 여부입니다.
+ */
+export const isSameMonth = (date1, date2) => {
+  const originalDateMonth = date1.getMonth();
+  const preparedDateMonth = date2.getMonth();
+
+  return originalDateMonth === preparedDateMonth;
+};
+
+/**
+ * 입력된 Date들이 같은 일인지 비교합니다.
+ * @param {Date} date1 원본 날짜입니다.
+ * @param {Date} date2 비교할 날짜입니다.
+ * @returns {boolean} 월의 동일 여부입니다.
+ */
+export const isSameDay = (date1, date2) => {
+  const originalDateDay = date1.getDate();
+  const preparedDateDay = date2.getDate();
+
+  return originalDateDay === preparedDateDay;
+};
+
+/**
+ * 입력된 Date들이 같은 날짜인지 비교합니다.
+ * @param {Date} date1 원본 날짜입니다.
+ * @param {Date} date2 비교할 날짜입니다.
+ * @returns {boolean} 날짜의 동일 여부입니다.
+ */
+export const isSameDate = (date1, date2) => {
+  const sameYear = isSameYear(date1, date2);
+  const sameMonth = isSameMonth(date1, date2);
+  const sameDate = isSameDay(date1, date2);
+
+  return sameYear && sameMonth && sameDate;
+};
