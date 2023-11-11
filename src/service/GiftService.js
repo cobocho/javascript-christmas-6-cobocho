278b1d48484d325411add44e2dd0db402cf00029
cobocho <rlaalsrb1111@naver.com>
2023-11-12 00:33:12 +0900

    refactor: 증정 이벤트 기간 상수화

--- src/service/GiftService.js
+++ src/service/GiftService.js
@@ -2,12 +2,21 @@ import { Receipt, Scheduler } from '../domain/index.js';
 
 const GiftService = Object.freeze({
   /**
+   * 증정 이벤트의 기간입니다.
+   * @readonly
+   */
+  EVENT_PERIOD: {
+    year: 2023,
+    month: 12,
+  },
+
+  /**
    * 총 가격에 따라 증정품을 부여합니다.
    * @param {Receipt} receipt - 증정품을 기록할 영수증입니다.
    */
   giveaway(receipt) {
     const giftEventScheduler = Scheduler.of();
-    giftEventScheduler.addEventMonth(2023, 12);
+    giftEventScheduler.addEventMonth(GiftService.EVENT_PERIOD.year, GiftService.EVENT_PERIOD.month);
     if (giftEventScheduler.isEventDate(receipt.getDate())) {
       receipt.receiveGiveaway();
     }
