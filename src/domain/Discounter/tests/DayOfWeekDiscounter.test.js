137c4ce7e610d673300d19c155113b0342678f10
cobocho <rlaalsrb1111@naver.com>
2023-11-12 20:54:25 +0900

    test: 평일 할인 테스트 코드 작성

--- src/domain/Discounter/tests/DayOfWeekDiscounter.test.js
+++ src/domain/Discounter/tests/DayOfWeekDiscounter.test.js
@@ -44,9 +44,157 @@ describe('DayOfWeekDiscounter 테스트', () => {
     receipt.orderMany(orders);
 
     // when
-    const { benefit } = dayOfWeekDiscounter.run(receipt);
+    const { name, benefit } = dayOfWeekDiscounter.run(receipt);
 
     // then
+    expect(name).toBe(DayOfWeekDiscounter.WEEKEND_EVENT_NAME);
     expect(benefit).toBe(discount);
+  });
+
+  it.each([
+    // 일요일
+    {
+      date: '2023-12-03',
+      orders: [{ name: '초코케이크', quantity: 1 }],
+      discount: 2_023,
+    },
+    {
+      date: '2023-12-03',
+      orders: [
+        { name: '초코케이크', quantity: 1 },
+        { name: '아이스크림', quantity: 1 },
+      ],
+      discount: 2_023 * 2,
+    },
+    // 월요일
+    {
+      date: '2023-12-04',
+      orders: [{ name: '초코케이크', quantity: 1 }],
+      discount: 2_023,
+    },
+    {
+      date: '2023-12-04',
+      orders: [
+        { name: '초코케이크', quantity: 1 },
+        { name: '아이스크림', quantity: 1 },
+      ],
+      discount: 2_023 * 2,
+    },
+    // 화요일
+    {
+      date: '2023-12-05',
+      orders: [{ name: '초코케이크', quantity: 1 }],
+      discount: 2_023,
+    },
+    {
+      date: '2023-12-05',
+      orders: [
+        { name: '초코케이크', quantity: 1 },
+        { name: '아이스크림', quantity: 1 },
+      ],
+      discount: 2_023 * 2,
+    },
+    // 수요일
+    {
+      date: '2023-12-06',
+      orders: [{ name: '초코케이크', quantity: 1 }],
+      discount: 2_023,
+    },
+    {
+      date: '2023-12-06',
+      orders: [
+        { name: '초코케이크', quantity: 1 },
+        { name: '아이스크림', quantity: 1 },
+      ],
+      discount: 2_023 * 2,
+    },
+    // 목요일
+    {
+      date: '2023-12-07',
+      orders: [{ name: '초코케이크', quantity: 1 }],
+      discount: 2_023,
+    },
+    {
+      date: '2023-12-07',
+      orders: [
+        { name: '초코케이크', quantity: 1 },
+        { name: '아이스크림', quantity: 1 },
+      ],
+      discount: 2_023 * 2,
+    },
+  ])('평일에는 디저트를 개당 2,023원 할인한다.', ({ date, orders, discount }) => {
+    // given
+    const receipt = Receipt.of(new Date(date));
+    receipt.orderMany(orders);
+
+    // when
+    const { name, benefit } = dayOfWeekDiscounter.run(receipt);
+
+    // then
+    expect(name).toBe(DayOfWeekDiscounter.WEEKDAY_EVENT_NAME);
+    expect(benefit).toBe(discount);
+  });
+
+  it.each([
+    // 금요일
+    {
+      date: '2023-12-01',
+      orders: [{ name: '양송이수프', quantity: 3 }],
+    },
+    {
+      date: '2023-12-01',
+      orders: [{ name: '초코케이크', quantity: 1 }],
+    },
+    {
+      date: '2023-12-01',
+      orders: [
+        { name: '제로콜라', quantity: 1 },
+        { name: '양송이수프', quantity: 2 },
+      ],
+    },
+    // 월요일
+    {
+      date: '2023-12-04',
+      orders: [{ name: '티본스테이크', quantity: 1 }],
+    },
+    {
+      date: '2023-12-04',
+      orders: [{ name: '양송이수프', quantity: 2 }],
+    },
+    {
+      date: '2023-12-04',
+      orders: [
+        { name: '제로콜라', quantity: 1 },
+        { name: '양송이수프', quantity: 2 },
+      ],
+    },
+  ])('요일 이벤트 적용대상이 아니면 할인이 되지 않는다.', ({ date, orders }) => {
+    // given
+    const receipt = Receipt.of(new Date(date));
+    receipt.orderMany(orders);
+
+    // when
+    const result = dayOfWeekDiscounter.run(receipt);
+
+    // then
+    expect(result).toBeNull();
+  });
+
+  it.each([
+    // 월요일
+    {
+      date: '2023-12-04',
+      orders: [{ name: '아이스크림', quantity: 1 }],
+    },
+  ])('10,000원 이하라면 할인이 되지 않는다.', ({ date, orders }) => {
+    // given
+    const receipt = Receipt.of(new Date(date));
+    receipt.orderMany(orders);
+
+    // when
+    const result = dayOfWeekDiscounter.run(receipt);
+
+    // then
+    expect(result).toBeNull();
   });
 });
