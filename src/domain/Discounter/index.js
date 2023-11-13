bd4d3c6f1db3f094b3dbf6f8bebd1b1f6d4c21ef
cobocho <rlaalsrb1111@naver.com>
2023-11-13 12:47:36 +0900

    feat: `SpecialDiscounter` 기능 구현

--- src/domain/Discounter/index.js
+++ src/domain/Discounter/index.js
@@ -1,3 +1,4 @@
 export { default as Discounter } from './Discounter.js';
 export { default as DDayDiscounter } from './DDayDiscounter.js';
 export { default as DayOfWeekDiscounter } from './DayOfWeekDiscounter.js';
+export { default as SpecialDiscounter } from './SpecialDiscounter.js';
