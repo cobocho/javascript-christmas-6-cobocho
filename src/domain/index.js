6b0c5c7618149ddab5f77a62b6e772f5fc3ec30d
cobocho <rlaalsrb1111@naver.com>
2023-11-13 13:55:54 +0900

    feat: 배지 기능 구현

--- src/domain/index.js
+++ src/domain/index.js
@@ -2,3 +2,4 @@ export { default as Receipt } from './Receipt/Receipt.js';
 export { default as OrderDetail } from './OrderDetail/OrderDetail.js';
 export { default as OrderTaker } from './OrderTaker/OrderTaker.js';
 export { default as Scheduler } from './Scheduler/Scheduler.js';
+export { default as Badge } from './Badge/Badge.js';
